e0bc3fbd060e05cccd66a8bd95f90697be8443ef
NilsMinor <nilsminor@web.de>
2018-12-12 21:08:24 +0100

    added on off and brightness functionality

--- pcbi/pcbi/pcbi.ino
+++ pcbi/pcbi/pcbi.ino
@@ -17,6 +17,17 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRBW + N
 
 
 
+// global variables
+float brightness = 0.5;
+bool on_off = false;
+
+
+void all_on (void);
+void all_off (void);
+void all_color (uint8_t r, uint8_t g, uint8_t b);
+void all_wwhite (uint8_t w);
+void all_leds (uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -25,7 +36,7 @@ void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   Serial.print("Connecting");
-  while (Blynk.connect() == false) {// Wait until connected
+  while (Blynk.connect() == false) { 
     
     Serial.print(".");
     delay(100);
@@ -33,12 +44,40 @@ void setup() {
   
   Serial.println();
   Blynk.syncAll();
-  //lcd.clear();
-  //lcd.print(0, 1, "Welcome Nils!");
+}
 
+BLYNK_WRITE(V0) {
+  on_off = (float)param.asInt();
+}
+
+BLYNK_WRITE(V2) {
+  brightness = (float)param.asInt()/100;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (on_off) 
+    all_leds (0,0,0,255);
+  else 
+    all_off ();
+    
+ 
+ 
+    delay(10);
+  Blynk.run();
 
+}
+
+void all_off (void) {
+   all_leds (0,0,0,0);
+}
+
+
+
+
+// set all leds with one command
+void all_leds (uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+  for (int i=0;i!=PIXEL_COUNT;i++) {
+      strip.setPixelColor(i, strip.Color(r*brightness, g*brightness, b*brightness,w*brightness));
+  }
+  strip.show();
 }
