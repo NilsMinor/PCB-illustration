9a83a79fe1e45eb28a13ab57e3b30f57096b333d
NilsMinor <nilsminor@web.de>
2018-12-16 21:46:03 +0100

    added first timer based effects (better delay :D) added rainbow and fire mode for testing prpose only

--- pcbi/pcbi/pcbi.ino
+++ pcbi/pcbi/pcbi.ino
@@ -34,32 +34,31 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <Adafruit_NeoPixel.h>
-#include <TimeLib.h>
-#include <WidgetRTC.h>
+
 
 #include "Seeed_CY8C401XX.h"    // Grove touch sensor
 #include "DHT.h"                // DHT22 temperature and humidity sensor
 
 // DHT 22 setting
-#define DHTPIN 13                // what digital pin we're connected to
+#define DHTPIN 13               // 
 #define DHTTYPE DHT22           // DHT 22  (AM2302), AM2321
 
 // LED strip related configs
 #define PIXEL_PIN   12          // Wemos D6 
-#define PIXEL_COUNT 180         // Pixels Count
-#define MODE_MAX    10          
+#define PIXEL_COUNT 159         // Pixels Count
+#define MODE_MAX    5           // max modes          
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRBW + NEO_KHZ800);
 
 CY8C touch;
 DHT dht(DHTPIN, DHTTYPE);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRBW + NEO_KHZ800);
 BlynkTimer timer;
-
+BlynkTimer ledTimer;
 
 // global variables
 float brightness = 0.5;
 bool on_off = false;
-uint8_t selected_mode = 1;
+uint8_t selected_mode = 4;
 uint8_t red, green, blue;
 
 
@@ -72,7 +71,10 @@ void all_leds (uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 void sendTemperatureHumidity(void);
 void handle_touch (void);
 void connectWLANIndicator (void);
-void nextMode (void);
+void nextMode (int v);
+void runLEDMode (void);
+
+void Fire(int Cooling, int Sparking, int SpeedDelay);
 
 void setup() {
   // put your setup code here, to run once:
@@ -88,7 +90,9 @@ void setup() {
 
   red = green = blue = 0;
 
-  timer.setInterval(1000L, sendTemperatureHumidity); // Setup a function to be called every second
+  timer.setInterval(1000L, sendTemperatureHumidity);  // Setup a function to be called every second
+  ledTimer.setInterval(100L, runLEDMode);             // timer for led modes/effects
+
   brightness = 0.5;
   Blynk.virtualWrite(V1, (int)(brightness * 100));   // set default brightness
   Blynk.virtualWrite(V3, selected_mode);             // set default mode   
@@ -106,7 +110,7 @@ BLYNK_WRITE(V1) {
 }
 
 BLYNK_WRITE(V2) {
-  nextMode ();
+  nextMode (param.asInt());
 }
 
 BLYNK_WRITE(V5) {
@@ -118,23 +122,47 @@ BLYNK_WRITE(V5) {
 //----------------------------------------------------------------------------------------------------
 
 void loop() {
+
+  if (on_off) {
+    switch (selected_mode) {
+
+      case 0:   // Normal (Warm White)
+      case 255:
+              all_leds(0, 0, 0, 255);
+        break;
+      
+      case 1: // Normal (Fading 1)
+              all_leds(0, 0, 0, 255);
+        break;
   
-  if (on_off) 
-    //all_leds (0,0,0,255);
-    all_leds(red,green,blue,0);
-  else 
-    all_off ();
+      case 2: // Normal (Fading 2)
+        break;
+        
+      case 3: // RGB (free-wheel)
+            all_leds(red, green, blue,0);
+        break;
+  
+      case 4: // RGB (rainbow)
+            runLEDMode ( );
+        break;
+
+      case 5: // RGB (Feuer)
+            rainbowCycle (100);
+        break;
+    }
+  }
     
-    handle_touch ();
+  else {
+    all_off ();
+  }
+    
+  handle_touch ();
 
-    //delay(10);
-    Blynk.run();
-    timer.run();
+  Blynk.run();
+  timer.run();
 }
 
-void all_off (void) {
-   all_leds (0,0,0,0);
-}
+
 
 // Connect to WLAN, wait while not conected
 void connectWLANIndicator (void) {
@@ -143,7 +171,11 @@ void connectWLANIndicator (void) {
 
   // wait for wlan connection
   Serial.print("Connecting");
-  single_led (10,0,0,200,0);
+  single_led (8,0,0,55,0);
+  single_led (9,0,0,155,0);
+  single_led (10,0,0,255,0);
+  single_led (11,0,0,155,0);
+  single_led (12,0,0,55,0);
   while (Blynk.connect() == false) { 
     Serial.print(".");
     delay(100); 
@@ -155,13 +187,14 @@ void connectWLANIndicator (void) {
 }
 
 // handle next mode (step) event
-void nextMode (void) {
-  selected_mode = ( selected_mode + 1 ) % MODE_MAX;
+void nextMode (int v) {
+  selected_mode = ( selected_mode + v ) % MODE_MAX;
   
   Serial.print("Mode : ");
   Serial.println(selected_mode);
   
   Blynk.virtualWrite(V3, selected_mode); 
+  
 }
 
 // handle touch events
@@ -194,7 +227,7 @@ void handle_touch (void) {
         pressed2 = true;
         Serial.println("button 1 is pressed");
   
-        nextMode ( );
+        nextMode (1); // +1
     }
     else if(!(value&0x02) && pressed2 == true) {   // button was released
         Serial.println("button 1 is released");
@@ -212,6 +245,8 @@ void handle_touch (void) {
       Serial.println(value);
     }
 }
+
+// update DHT22 temperature and humidity values
 void sendTemperatureHumidity(void) {
   
   float h = dht.readHumidity();
@@ -227,6 +262,24 @@ void sendTemperatureHumidity(void) {
 }
 
 
+// ------------------------------------------------------------
+void runLEDMode (void) {
+  switch (selected_mode) {
+ 
+      case 4: // RGB (rainbow)
+           rainbowCycle (100);
+        break;
+
+      case 5: // RGB (Feuer)
+            Fire(55,120,15);
+        break;
+    }
+}
+void all_off (void) {
+   Serial.println("All LEDs off");
+   all_leds (0,0,0,0);
+}
+
 // set all leds with one command
 void all_leds (uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
   for (int i=0;i!=PIXEL_COUNT;i++) {
@@ -238,4 +291,92 @@ void all_leds (uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
 void single_led (uint8_t pin, uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
   strip.setPixelColor(pin, strip.Color(r*brightness, g*brightness, b*brightness,w*brightness));
   strip.show();
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle (uint8_t wait) {
+  uint16_t i, j;
+  Serial.println("Vor loop");
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      //Serial.println(i);
+    }
+    strip.show();
+    //delay(wait);
+    Blynk.run();   
+  }
+  Serial.println("Nach loop");
+}
+
+
+
+void setPixelHeatColor (int Pixel, byte temperature) {
+  // Scale 'heat' down from 0-255 to 0-191
+  byte t192 = round((temperature/255.0)*191);
+ 
+  // calculate ramp up from
+  byte heatramp = t192 & 0x3F; // 0..63
+  heatramp <<= 2; // scale up to 0..252
+ 
+  // figure out which third of the spectrum we're in:
+  if( t192 > 0x80) {                     // hottest
+    strip.setPixelColor(Pixel, 255, 255, heatramp);
+  } else if( t192 > 0x40 ) {             // middle
+    strip.setPixelColor(Pixel, 255, heatramp, 0);
+  } else {                               // coolest
+    strip.setPixelColor(Pixel, heatramp, 0, 0);
+  }
+}
+
+void Fire(int Cooling, int Sparking, int SpeedDelay) {
+  static byte heat[PIXEL_COUNT];
+  int cooldown;
+  
+  // Step 1.  Cool down every cell a little
+  for( int i = 0; i < PIXEL_COUNT; i++) {
+    cooldown = random(0, ((Cooling * 10) / PIXEL_COUNT) + 2);
+    
+    if(cooldown>heat[i]) {
+      heat[i]=0;
+    } else {
+      heat[i]=heat[i]-cooldown;
+    }
+  }
+  
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( int k= PIXEL_COUNT - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  }
+    
+  // Step 3.  Randomly ignite new 'sparks' near the bottom
+  if( random(255) < Sparking ) {
+    int y = random(7);
+    heat[y] = heat[y] + random(160,255);
+    //heat[y] = random(160,255);
+  }
+
+  // Step 4.  Convert heat to LED colors
+  for( int j = 0; j < PIXEL_COUNT; j++) {
+    setPixelHeatColor(j, heat[j] );
+  }
+
+  strip.show();
+  //delay(SpeedDelay);
 }
