b9dbcef098ccb0e0396b7ddb15d05f85e4a12534
NilsMinor <nilsminor@web.de>
2018-12-16 18:39:08 +0100

    added wlan indicator on start sequence

--- pcbi/pcbi/pcbi.ino
+++ pcbi/pcbi/pcbi.ino
@@ -26,13 +26,13 @@
 #include "DHT.h"                // DHT22 temperature and humidity sensor
 
 // DHT 22 setting
-#define DHTPIN 2                // what digital pin we're connected to
+#define DHTPIN 13                // what digital pin we're connected to
 #define DHTTYPE DHT22           // DHT 22  (AM2302), AM2321
 
 
 // LED strip related configs
-#define PIXEL_PIN   12     // Wemos D6 
-#define PIXEL_COUNT 200    // Pixels Count
+#define PIXEL_PIN   12          // Wemos D6 
+#define PIXEL_COUNT 200         // Pixels Count
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRBW + NEO_KHZ800);
 
@@ -50,31 +50,31 @@ void all_on (void);
 void all_off (void);
 void all_color (uint8_t r, uint8_t g, uint8_t b);
 void all_wwhite (uint8_t w);
+void single_led (uint8_t pin, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 void all_leds (uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 void sendTemperatureHumidity(void);
 void handle_touch (void);
+void connectWLANIndicator (void);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   touch.init();
   Blynk.begin(auth, wlan_name, wlan_pw);
 
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  Serial.print("Connecting");
-  while (Blynk.connect() == false) { 
-    
-    Serial.print(".");
-    delay(100);
-  } 
-  Serial.println();
-
   dht.begin();
-  timer.setInterval(1000, sendTemperatureHumidity);// Setup a function to be called every second
+
+  connectWLANIndicator ();
+
+  
+  timer.setInterval(1000L, sendTemperatureHumidity); // Setup a function to be called every second
 
   Blynk.syncAll();  
 }
+
+//----------------------------------------------------------------------------------------------------
+
 
 BLYNK_WRITE(V0) {
   on_off = (float)param.asInt();
@@ -84,20 +84,42 @@ BLYNK_WRITE(V1) {
   brightness = (float)param.asInt()/100;
 }
 
+//----------------------------------------------------------------------------------------------------
+
 void loop() {
+  
   if (on_off) 
     all_leds (0,0,0,255);
   else 
     all_off ();
     
-    handle_touch ();
- 
-    delay(10);
+    //handle_touch ();
+
+    //delay(10);
     Blynk.run();
+    timer.run();
 }
 
 void all_off (void) {
    all_leds (0,0,0,0);
+}
+
+// Connect to WLAN, wait while not conected
+void connectWLANIndicator (void) {
+  
+  all_off ();
+
+  // wait for wlan connection
+  Serial.print("Connecting");
+  single_led (10,0,0,200,0);
+  while (Blynk.connect() == false) { 
+    Serial.print(".");
+    delay(100); 
+  } 
+  Serial.println();
+
+  Serial.print("connected");
+  all_off ();
 }
 
 
@@ -127,20 +149,23 @@ void handle_touch (void) {
 
     // notify blynk
 }
-
-void sendTemperatureHumidity(void)
-{
-  float h = dht.readHumidity();
+void sendTemperatureHumidity(void) {
+  
+  /*float h = dht.readHumidity();
   float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
 
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+  
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);*/
 
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V10, h);
-  Blynk.virtualWrite(V11, t);
+  Serial.println("Test DHT sensor!");
+
+  Blynk.virtualWrite(V5, 1.23);
+  Blynk.virtualWrite(V6, 45.67);
 }
 
 
@@ -149,5 +174,10 @@ void all_leds (uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
   for (int i=0;i!=PIXEL_COUNT;i++) {
       strip.setPixelColor(i, strip.Color(r*brightness, g*brightness, b*brightness,w*brightness));
   }
+  strip.show();
+}
+
+void single_led (uint8_t pin, uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+  strip.setPixelColor(pin, strip.Color(r*brightness, g*brightness, b*brightness,w*brightness));
   strip.show();
 }
