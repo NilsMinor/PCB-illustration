d0824285b91dd6ac54bd384167b5579301340bf2
NilsMinor <nilsminor@web.de>
2018-12-31 12:57:09 +0100

    implmented ir code resolver for first remote

--- pcbi/pcbi/pcbi_ir.cpp
+++ pcbi/pcbi/pcbi_ir.cpp
@@ -2,7 +2,8 @@
 
 #include "pcbi_ir.h"
 
-
+                    //    ON         OFF      L_UP     L_DOWN      RED     GREEN      BLUE      WHITE
+uint64_t ir_one [] = { 0xFFB04F, 0xFFF807, 0xFF906F, 0xFFB847, 0xFF9867, 0xFFD827, 0xFF8877, 0xFFA857  };
 
 RGBRemoteController::RGBRemoteController () {
   irrecv = new IRrecv (IR_PIN);
@@ -13,14 +14,23 @@ RGBRemoteController::RGBRemoteController () {
 bool RGBRemoteController::checkIRRemote (void) {
   if (irrecv->decode(&results)) {
     // print() & println() can't handle printing long longs. (uint64_t)
-    serialPrintUint64(results.value, HEX);
-    Serial.println("");
+    ir_codes_pos code = translateIRCode();
+    Serial.print ("IR Code nr: ");
+    Serial.println (code);
+
     irrecv->resume();  // Receive the next value
   }
 }
 
 
+// Translate code 
+ir_codes_pos RGBRemoteController::translateIRCode (void) {
+  for (int code = 0; code != IR_CODES; code++) {
+    // compare codes with the stored codes of the remote 
+    if (results.value == ir_one[code]) {
 
-void RGBRemoteController::translateIRCode (void) {
-  
+      return (ir_codes_pos) code;
+    }
+  }
+  return ir_error;
 }
--- pcbi/pcbi/pcbi_ir.h
+++ pcbi/pcbi/pcbi_ir.h
@@ -1,6 +1,6 @@
 
 /* Class to control the pcbi panel with common RGB infrared remotes like this(https://www.ebay.de/itm/401420892888)
- * Based on IRrecvDumpV2 from David Conran and Ken Shirriff.  
+ * Based on IRrecvDumpV2 from David Conran and Ken Shirri0xFF.  
  * 
  * My setup uses an TSOP-31238 (https://cdn-reichelt.de/documents/datenblatt/A500/TSOP312SERIES_DATASHEET.pdf) with 38kHz
  *
@@ -13,22 +13,32 @@
 #include <IRrecv.h>
 #include <IRremoteESP8266.h>
 #include <IRutils.h>
-
 #include "Arduino.h"
 
+#define IR_PIN          2
+#define IR_CODES        8
 
-#define IR_PIN                2
+enum ir_codes_pos {
+  ir_error = -1,
+  ir_on = 0, 
+  ir_off, 
+  ir_lup,
+  ir_ldown,
+  ir_red,
+  ir_green,
+  ir_blue,
+  ir_white
+  };
 
 
 class RGBRemoteController{
   public:
     RGBRemoteController ();
     bool checkIRRemote (void);
-    bool isIdle (void);
 
   private:
     
-    void translateIRCode (void);
+    ir_codes_pos translateIRCode (void);
     
     IRrecv *irrecv;
     decode_results results;  // Somewhere to store the results 
